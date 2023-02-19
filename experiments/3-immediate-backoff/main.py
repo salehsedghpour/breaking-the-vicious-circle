c7e63fc6641b0e77eb9d1c3afee5bb5cb1e79f6b
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-02-19 12:26:09 +0100

    add the experiments for cb-none-interval-25

--- experiments/3-immediate-backoff/main.py
+++ experiments/3-immediate-backoff/main.py
@@ -19,7 +19,7 @@ functions.k8s_authentication()
 CB_values =[None]#'dynamic', 1, 50, None]
 retry_values = ['dynamic', [2, 10], None]
 traffic_patterns = ['static', ]#'spike']
-output_log_file_name = functions.get_project_root()+'/logs/exp-3-cb-none-interval-1ms.csv'
+output_log_file_name = functions.get_project_root()+'/logs/exp-3-cb-none-interval-25ms.csv'
 deployment_list = ['adservice-dep', 'cartservice-dep', 'checkoutservice-dep', 'currencyservice-dep', 'emailservice-dep',
                     'frontend-dep', 'paymentservice-dep', 'productcatalogservice-dep', 'recommendationservice-dep',
                     'redis-cart-dep', 'shippingservice-dep']
@@ -163,7 +163,7 @@ def create_retry(service_name, namespace, retry_attempt ):
                     ],
                     "retries": {
                         "attempts": retry_attempt,
-                        "perTryTimeout": "1ms",
+                        "perTryTimeout": "25ms",
                         "retryOn": "connect-failure,refused-stream,unavailable,cancelled,retriable-status-codes,5xx,deadline-exceeded"
                     },
                 }
@@ -195,7 +195,7 @@ def delete_retry(service_name, namespace, retry_attempt ):
                     ],
                     "retries": {
                         "attempts": retry_attempt,
-                        "perTryTimeout": "1ms",
+                        "perTryTimeout": "25ms",
                         "retryOn": "connect-failure,refused-stream,unavailable,cancelled,retriable-status-codes,5xx,deadline-exceeded"
                     },
                 }
@@ -378,7 +378,7 @@ def enforce_retry_controller():
                     ],
                     "retries": {
                         "attempts": retry_controller.retry_attempt,
-                        "perTryTimeout": "1ms",
+                        "perTryTimeout": "25ms",
                         "retryOn": "connect-failure,refused-stream,unavailable,cancelled,retriable-status-codes,5xx,deadline-exceeded"
                     },
                 }
